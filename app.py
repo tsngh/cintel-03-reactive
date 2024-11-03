de5c2b47af91b4799b8e215f958b08d522cc6458
tsngh <142849593+tsngh@users.noreply.github.com>
2024-11-03 14:37:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from shiny.express import input, ui
 from shinywidgets import render_plotly
 from shiny import render
 import seaborn as sns
+from shiny import reactive
 from palmerpenguins import load_penguins
 
 # Load the penguins dataset
@@ -86,3 +87,16 @@ with ui.card(full_screen=True):
                 input.selected_attributes(): f"{input.selected_attributes().replace('_', ' ').title()} (mm)"
             }
         )
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
