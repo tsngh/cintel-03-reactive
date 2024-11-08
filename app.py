4cddfa4b8be014d4cd181795c08e752629245133
tsngh <142849593+tsngh@users.noreply.github.com>
2024-11-07 18:10:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ with ui.card(full_screen=True):
 
 @reactive.calc
 def filtered_data():
-    return penguins_df
+    selected_species = input.selected_species_list()
+    return penguins_df[penguins_df["species"].isin(selected_species)]
